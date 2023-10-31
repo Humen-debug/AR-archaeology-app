d88aa72dc632e97008059d76fb3c35d503dcaec6
humenchau <humenchau@gmail.com>
2023-10-31 17:52:44 +0800

    change app ID

--- components/model_view.tsx
+++ components/model_view.tsx
@@ -153,25 +153,24 @@ export default function ModelView(props: ModelViewProps) {
     } finally {
       props.setLoading(false);
     }
-    // dev
-    if (Platform.OS === "ios") {
-      try {
-        const geometry = new THREE.BoxGeometry(1, 1, 1);
-        const material = new MeshBasicMaterial({ color: "white" });
-        const cube = new THREE.Mesh(geometry, material);
-        scene.add(cube);
-      } catch (error) {
-        console.log(error);
-        props.setError(error);
-      }
-    }
+    // // dev
+    // if (Platform.OS === "ios") {
+    //   try {
+    //     const geometry = new THREE.BoxGeometry(1, 1, 1);
+    //     const material = new MeshBasicMaterial({ color: "white" });
+    //     const cube = new THREE.Mesh(geometry, material);
+    //     scene.add(cube);
+    //   } catch (error) {
+    //     console.log(error);
+    //     props.setError(error);
+    //   }
+    // }
 
     // create render function
     const render = () => {
       timeout = requestAnimationFrame(render);
       if (obj) {
         obj.rotation.y += 0.01;
-        console.log("set rotate");
       }
       renderer.render(scene, camera);
       gl.endFrameEXP();
