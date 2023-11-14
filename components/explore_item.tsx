6b9163e1642db3a83537786e95fd06659044704e
Ada-Au <adaau0209@gmail.com>
2023-11-14 18:39:33 +0800

    added UsageDescription for ios, bottom modal

---
+++ components/explore_item.tsx
@@ -0,0 +1,115 @@
+import { Text, Button } from "react-native-paper";
+import { StyleSheet, View, FlatList, Image } from "react-native";
+import { Dimensions } from "react-native";
+import { useAppTheme } from "../styles";
+import GPSIcon from "../assets/icons/gps.svg";
+import BookMarkOutlineIcon from "../assets/icons/bookmark-outline.svg";
+import BookMarkIcon from "../assets/icons/bookmark.svg";
+
+interface ItemProps {
+  title: string;
+  length: number; // in km
+  isSaved: boolean;
+  images?: string[] | null;
+}
+
+export default function ExploreItem(item: ItemProps) {
+  const { title, length, isSaved, images } = item;
+  const theme = useAppTheme();
+
+  const itemWidth = 270;
+  const window = { height: Dimensions.get("window").height, width: Dimensions.get("window").width };
+  const _style = useStyle({
+    backgroundColor: theme.colors.grey4,
+    labelGrey: theme.colors.label,
+    spacing: theme.spacing,
+    itemWidth,
+    window,
+    withImage: !!images,
+  });
+
+  return (
+    <View style={_style.item}>
+      <>
+        <Text variant="labelMedium">{title}</Text>
+        <Text variant="labelSmall" style={_style.lengthText}>
+          {length} km
+        </Text>
+        {images && (
+          <FlatList
+            horizontal
+            ItemSeparatorComponent={() => <View style={{ width: theme.spacing.sm }} />}
+            data={images}
+            renderItem={({ item }) => (
+              <Image
+                style={_style.image}
+                source={{
+                  uri: item,
+                }}
+              />
+            )}
+          />
+        )}
+      </>
+      <View style={{ display: "flex", flexDirection: "row" }}>
+        <Button
+          compact
+          labelStyle={_style.button}
+          textColor={theme.colors.grey1}
+          mode="contained"
+          icon={() => <GPSIcon fill="white" style={_style.icon} />}
+        >
+          <Text variant="labelSmall">Start</Text>
+        </Button>
+        <Button
+          compact
+          labelStyle={_style.button}
+          mode="outlined"
+          icon={() =>
+            isSaved ? (
+              <BookMarkIcon style={_style.icon} fill={theme.colors.primary} />
+            ) : (
+              <BookMarkOutlineIcon style={_style.icon} fill={theme.colors.primary} />
+            )
+          }
+          style={{ marginLeft: 8, borderColor: theme.colors.primary }}
+        >
+          <Text variant="labelSmall" style={{ color: theme.colors.primary }}>
+            {isSaved ? "Saved" : "Save"}
+          </Text>
+        </Button>
+      </View>
+    </View>
+  );
+}
+
+const useStyle = ({ backgroundColor, spacing, itemWidth, labelGrey, window, withImage }: any) =>
+  StyleSheet.create({
+    image: {
+      height: 100,
+      width: 160,
+      borderRadius: 8,
+      marginTop: spacing.xs,
+      marginBottom: spacing.sm,
+    },
+    item: {
+      backgroundColor: backgroundColor,
+      height: withImage ? undefined : 96,
+      borderRadius: spacing.xs,
+      paddingHorizontal: spacing.lg,
+      paddingVertical: spacing.sm,
+      justifyContent: "space-between",
+    },
+    button: {
+      marginVertical: 4,
+      fontSize: 8,
+    },
+    icon: {
+      maxHeight: 16,
+      marginHorizontal: -6,
+    },
+    lengthText: {
+      color: labelGrey,
+      marginTop: withImage ? 0 : -9,
+    },
+  });
---
+++ components/explore_modal.tsx
@@ -0,0 +1,114 @@
+import { Text, Searchbar, Divider } from "react-native-paper";
+import { StyleSheet, View, TouchableOpacity } from "react-native";
+import { useAppTheme } from "../styles";
+import { useRef, useMemo, useCallback, useState, useEffect } from "react";
+import { BottomSheetModal, BottomSheetScrollView, BottomSheetScrollViewMethods } from "@gorhom/bottom-sheet";
+import SearchIcon from "../assets/icons/search.svg";
+import ExploreItem from "./explore_item";
+
+export default function ExploreModal({ open, setOpen, data }: any) {
+  const theme = useAppTheme();
+  const _style = useStyle({
+    spacing: theme.spacing,
+  });
+
+  const bottomSheetRef = useRef<BottomSheetModal>(null);
+  //   const bottomSheetScrollRef = useRef<BottomSheetScrollViewMethods | null>(null);
+  const snapPoints = useMemo(() => ["75%", "75%"], []);
+
+  const [searchText, setSearchText] = useState("");
+
+  useEffect(() => {
+    if (open) {
+      modalOpen();
+    }
+  }, [open]);
+
+  const modalOpen = useCallback(() => {
+    bottomSheetRef.current?.present();
+  }, []);
+
+  const modalCLose = useCallback(() => {
+    bottomSheetRef.current?.dismiss();
+  }, []);
+
+  const modalToggle = () => {
+    if (open) {
+      modalCLose();
+    } else {
+      setOpen(true);
+      modalOpen();
+    }
+  };
+
+  const onSubmit = () => {
+    console.log("filterList", searchText);
+  };
+
+  if (!open) return <></>;
+  return (
+    <View style={{ ..._style.fill, position: "absolute" }}>
+      {open && <TouchableOpacity style={_style.fill} onPress={modalToggle} />}
+      <BottomSheetModal
+        backgroundStyle={{ backgroundColor: theme.colors.grey4 }}
+        enablePanDownToClose
+        ref={bottomSheetRef}
+        index={1}
+        snapPoints={snapPoints}
+        onDismiss={() => setOpen(false)}
+      >
+        <View style={{ paddingHorizontal: theme.spacing.lg }}>
+          <Searchbar
+            placeholder="Search"
+            value={searchText}
+            onChangeText={setSearchText}
+            onSubmitEditing={onSubmit}
+            mode="bar"
+            icon={() => null}
+            elevation={0}
+            style={_style.searchbar}
+            iconColor={theme.colors.grey1}
+            traileringIconColor={theme.colors.grey1}
+            traileringIcon={({ color }) => <SearchIcon fill={color || theme.colors.grey1} />}
+          />
+          <Divider style={{ backgroundColor: "black", marginHorizontal: -theme.spacing.lg }} />
+        </View>
+        <BottomSheetScrollView contentContainerStyle={{ flex: 1 }} showsVerticalScrollIndicator={false}>
+          <View style={_style.list}>
+            {data.map(({ title, length, save, images }) => (
+              <>
+                <ExploreItem title={title} length={length} isSaved={save} images={images} />
+                <Divider style={{ backgroundColor: "black", marginHorizontal: -theme.spacing.lg }} />
+              </>
+            ))}
+          </View>
+        </BottomSheetScrollView>
+      </BottomSheetModal>
+    </View>
+  );
+}
+
+const useStyle = ({ spacing }: any) =>
+  StyleSheet.create({
+    searchbar: {
+      flexGrow: 1,
+      flexShrink: 1,
+      backgroundColor: "#6D6D6D33",
+      borderRadius: 4,
+      marginHorizontal: spacing.lg,
+      marginBottom: spacing.lg,
+    },
+    fill: {
+      height: "100%",
+      width: "100%",
+    },
+    list: {
+      flex: 1,
+      overflow: "hidden",
+      flexDirection: "column",
+    },
+    item: {
+      paddingVertical: spacing.md,
+      paddingHorizontal: spacing.lg,
+    },
+  });
